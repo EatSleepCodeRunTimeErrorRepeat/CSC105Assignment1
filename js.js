5ee8deaca9d92753c220d77542724cdb19013123
EatSleepCodeRunTimeErrorRepeat <ryan.letchman@mail.kmutt.ac.th>
2023-02-09 21:33:22 +0700

    1st upload for the Assignment

---
+++ js.js
@@ -0,0 +1,11 @@
+
+
+function myMacaroni() {
+  alert("You clicked the Chicken Macaroni!")
+}
+
+function myPeanutbutter() {
+  alert("You clicked the Peanut Butter Sandwich!")
+}
+
+//will complete the webpage later
